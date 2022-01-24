45c65397a5e6492a0b55843e0f3b0a6045c867bf
Jang-Ahyoung <wegotthelove27@gmail.com>
2022-01-24 20:47:24 +0900

    FEAT : 식단 간 가독성 개서

--- src/components/meal/meal.jsx
+++ src/components/meal/meal.jsx
@@ -1,15 +1,17 @@
-import React,{memo} from 'react';
-import styles from './meal.module.css';
+import React from "react";
+import styles from "./meal.module.css";
 
-const Meal = memo((props) => {
-    
-    return(
+
+const Meal = ({ meal }) => {
+
+  return (
     <div>
-        <span className={styles.per_meal}>
-            <div className={styles.time}><b>{props.meal.codeNm}</b></div>
-            <div className={styles.meal}>{props.meal.mealNm}</div>
-        </span>
-    </div>    
-)});
+      <span className={styles.per_meal}>
+        <div className={styles.meal_kind}><b>{meal.codeNm}</b></div>
+        <div className={styles.meal}>{meal.mealNm.split("/").join(", ")}</div>
+      </span>
+    </div>
+  );
+};
 
 export default Meal;
